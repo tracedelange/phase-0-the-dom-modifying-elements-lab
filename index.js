2ce0e06af4544b3e41f15a3a3a7f7ed72c90fd90
Trace <tracedelange@me.com>
2021-06-01 16:59:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const mainElement = document.querySelector("main")
+mainElement.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Trace is the champion"
+
+document.body.appendChild(newHeader)
